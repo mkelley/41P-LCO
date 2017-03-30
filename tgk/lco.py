e8920c9e1bf8f5717df323d8aea3db64249aff3e
Michael S. P. Kelley <msk@astro.umd.edu>
2017-03-30 09:37:16 -0400

    Observation meta data, including JPL/HORIZONS parameters.

---
+++ tgk/lco.py
@@ -0,0 +1,22 @@
+# Licensed under a MIT style license - see LICENSE
+"""lco - Las Cumbres Observatory info and parameters."""
+
+# https://lco.global/observatory/mpccodes/
+mpc_codes = {
+    ('ogg', 'clma', '2m0a'): 'F65',
+    ('ogg', 'clma', '0m4b'): 'T04',
+    ('elp', 'doma', '1m0a'): 'V37',
+    ('lsc', 'doma', '1m0a'): 'W85',
+    ('lsc', 'domb', '1m0a'): 'W86',
+    ('lsc', 'domc', '1m0a'): 'W87',
+    ('lsc', 'aqwa', '0m4a'): 'W89',
+    ('cpt', 'doma', '1m0a'): 'K91',
+    ('cpt', 'domb', '1m0a'): 'K92',
+    ('cpt', 'domc', '1m0a'): 'K93',
+    ('coj', 'clma', '0m4b'): 'Q59',
+    ('coj', 'doma', '1m0a'): 'Q63',
+    ('coj', 'domb', '1m0a'): 'Q64',
+    ('coj', 'clma', '2m0a'): 'E10',
+    ('tfn', 'aqwa', '0m4a'): 'Z21',
+    ('tlv', 'doma', '1m0a'): '097',
+}
--- tgk/science.py
+++ tgk/science.py
@@ -1,6 +1,9 @@
 # Licensed under a MIT style license - see LICENSE
 """science - Comet science with LCO data."""
 
+import numpy as np
+import astropy.units as u
+from astropy.coordinates import Angle
 from .core import TGKMaster
 
 class Science(TGKMaster):
@@ -82,13 +85,12 @@ class Science(TGKMaster):
         i = 0
         unique = []
         duplicates = []
-
         while i < len(files):
             # files are sorted by frame name; how many are the same?
             found = []
             basename = files[i][0]
             while i < len(files) and basename in files[i][0]:
-                found.append(files[i][1])
+                found.append(files[i])
                 i += 1
 
             if len(found) == 1:
@@ -109,7 +111,12 @@ class Science(TGKMaster):
         return new_data
 
     def process(self, files):
-        pass
+        from astropy.io import fits
+        
+        for frame, rlevel, filename in files:
+            self.logger.info(frame)
+            with fits.open(filename) as hdu:
+                obs = Observation(hdu['sci'].header)
 
     def read_processing_history(self):
         """Read in processing history file."""
@@ -124,3 +131,258 @@ class Science(TGKMaster):
                     frame, rlevel, minions = line.split(';')
                     minions = minions.split(',')
                     self.processing_history[frame] = (rlevel, minions)
+
+class Observation:
+    """Meta data for each LCO frame.
+
+    Parameters
+    ----------
+    header : astropy.io.fits.Header
+      The FITS header for the frame in question.
+
+    Attributes
+    ----------
+    exptime : Quantity
+      Exposure time.
+    filter : string
+      Filter used.
+    rlevel : int
+      Reduction level.
+
+    radec_predict : astropy.coordinates.SkyCoord
+      Predectied J2000 coordinates (JPL/HORIZONS).
+    mu : Quantity
+      Proper motion  (JPL/HORIZONS).
+    mu_ra : Qunatity
+      Right ascention rate of change times cos(declination) (JPL/HORIZONS).
+    mu_dec : Quantity
+      Declination rate of change (JPL/HORIZONS).
+
+    rh : Quantity
+      Heliocentric distance (JPL/HORIZONS).
+    delta : Quantity
+      Observer-comet distance (JPL/HORIZONS).
+    phase : Angle
+      Phase angle (JPL/HORIZONS).
+
+    sun_position_angle : Angle
+      Projected comet->Sun vector.
+    velocity_position_angle : Angle
+      Projected comet velocity vector.
+
+    time : Time
+      Mid-point of observation.
+    start : Time
+      Start time of observation.
+    stop : Time
+      Stop time of observation.
+    wcs : astropy.wcs.WCS
+      The world coordinate system.
+
+    gain : Quantity
+      Detector gain.
+    nonlinearity : Quantity
+      Non-linearity limit.
+    pixel_scale : Quantity
+      Nominal pixel scale.
+    readnoise : Quantity
+      Detector readnoise.
+    trim : tuple of slices
+      Section of useful data.
+
+    site : tuple of strings
+      The observatory site ID and name.
+    enclosure : tuple of strings
+      The enclosure ID and name.
+    telescope : tuple of strings
+      The telescope ID and name.
+    instrument : string
+      The instrument ID.
+
+    longitude : Angle
+      Observatory longitude (East).
+    latitude : Angle
+      Observatory latitude.
+    elevation : Quantity
+      Observatory elevation.
+
+    lunar_alt : Angle
+      Lunar altitude.
+    lunar_elong : Angle
+      Lunar elongation.
+    solar_alt : Angle
+      Solar altitude.
+    solar_elong : Angle
+      Solar elongation.
+
+    
+
+    """
+
+    def __init__(self, header):        
+        self.header = header
+        self.get_ephemeris()
+
+    def get_ephemeris(self):
+        import callhorizons
+        from . import lco
+
+        q = callhorizons.query('41P')
+        q.set_discreteepochs([self.time.jd])
+        obs_code = lco.mpc_codes[(self.site[0], self.enclosure[0], self.telescope[0])]
+        n = q.get_ephemerides(obs_code)
+        if n != 1:
+            raise EphemerisError('Bad return from JPL/HORIZONS, check URL: {}'.format(q.url))
+        self._horizons_query = q
+
+    ############################################################
+    @property
+    def exptime(self):
+        return self.header['EXPTIME'] * u.s
+
+    @property
+    def filter(self):
+        return self.header['FILTER']
+
+    @property
+    def rlevel(self):
+        return self.header['RLEVEL']
+
+    @property
+    def airmass(self):
+        return self.header['AIRMASS']
+
+    ############################################################
+    @property
+    def radec_predict(self):
+        from astropy.coordinates import SkyCoord
+        ra = self._horizons_query['RA'][0] * u.deg
+        dec = self._horizons_query['DEC'][0] * u.deg
+        return SkyCoord(ra, dec)
+
+    @property
+    def mu(self):
+        return np.sqrt(self.mu_ra**2 + self.mu_dec**2)
+
+    @property
+    def mu_ra(self):
+        return self._horizons_query['RA_rate'][0] * u.arcsec / u.s
+
+    @property
+    def mu_dec(self):
+        return self._horizons_query['DEC_rate'][0] * u.arcsec / u.s
+
+    ############################################################
+    @property
+    def rh(self):
+        return self._horizons_query['r'][0] * u.au
+
+    @property
+    def delta(self):
+        return self._horizons_query['delta'][0] * u.au
+
+    @property
+    def phase(self):
+        return Angle(self._horizons_query['alpha'][0] * u.deg)
+
+    ############################################################
+    @property
+    def sun_position_angle(self):
+        return Angle((self._horizons_query['sunTargetPA'][0] + 180) * u.deg)
+
+    @property
+    def velocity_position_angle(self):
+        return Angle((self._horizons_query['velocityPA'][0] + 180) * u.deg)
+
+    ############################################################
+    @property
+    def site(self):
+        return (self.header['SITEID'], self.header['SITE'])
+
+    @property
+    def enclosure(self):
+        return (self.header['ENCID'], self.header['ENCLOSUR'])
+
+    @property
+    def telescope(self):
+        return (self.header['TELID'], self.header['TELESCOP'])
+
+    @property
+    def instrument(self):
+        return self.header['INSTRUME']
+
+    ############################################################
+    @property
+    def time(self):
+        from astropy.time import Time
+        return Time(self.header['DATE-OBS'], scale='utc')
+
+    @property
+    def start(self):
+        from astropy.time import Time
+        return Time(self.header['DATE-OBS'], scale='utc')
+
+    @property
+    def stop(self):
+        from astropy.time import Time
+        t = self.header['DATE-OBS'][:11] + self.header['UTSTOP']
+        return Time(t, scale='utc')
+
+    @property
+    def wcs(self):
+        from astropy.wcs import WCS
+        return WCS(self.header)
+    
+    ############################################################
+    @property
+    def gain(self):
+        return self.header['GAIN'] * u.electron / u.adu
+
+    @property
+    def nonlinearity(self):
+        return self.header['MAXLIN'] * u.adu
+
+    @property
+    def pixel_scale(self):
+        return self.header['PIXSCALE'] * u.arcsec
+    
+    @property
+    def readnoise(self):
+        return self.header['RDNOISE'] * u.electron
+
+    @property
+    def trimsec(self):
+        x, y = self.header['TRIMSEC'][1:-1].split(',')
+        x1, x2 = x.split(':')
+        y1, y2 = y.split(':')
+        return np.s_[(y1 - 1):y2, (x1 - 1):x2]
+    
+    ############################################################
+    @property
+    def latitude(self):
+        Angle(self.header['LATITUDE'] * u.deg)
+
+    @property
+    def longitude(self):
+        Angle(self.header['LONGITUD'] * u.deg)
+
+    @property
+    def elevation(self):
+        return self.header['HEIGHT'] * u.m
+
+    ############################################################
+    @property
+    def lunar_alt(self):
+        return Angle(self.header['MOONALT'] * u.deg)
+
+    @property
+    def lunar_elong(self):
+        return Angle(self.header['MOONDIST'] * u.deg)
+
+    @property
+    def solar_alt(self):
+        return Angle(self.header['SUNALT'] * u.deg)
+
+    @property
+    def solar_elong(self):
+        return Angle(self.header['SUNDIST'] * u.deg)
