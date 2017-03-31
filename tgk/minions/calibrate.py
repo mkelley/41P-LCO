e458741526e15dc2c116045480f26eec73f8696b
Michael S. P. Kelley <msk@astro.umd.edu>
2017-03-31 10:02:25 -0400

    Download appropriate data from PS1 catalog at STScI.

--- tgk/minions/calibrate.py
+++ tgk/minions/calibrate.py
@@ -6,6 +6,9 @@ from . import FrameMinion
 class Calibrate(FrameMinion):
     """Calibrate with PanSTARRS
 
+    VO Simple Cone Search parameters at:
+      https://archive.stsci.edu/vo/general_params.html
+
     Parameters
     ----------
     config : dict
@@ -24,24 +27,64 @@ class Calibrate(FrameMinion):
 
     def run(self):
         import os
-        import astropy.units as u
-        from astropy.io import votable
         import logging
         import requests
+        import astropy.units as u
+        from astropy.io import votable
 
         fn = self.minion_file('{}.xml'.format(self.obs.frame_name))
         if not os.path.exists(fn):
             self.logger.info('    Retrieving PS1 catalog for {} .'.format(self.obs.frame_name))
-            r = max(self.im.data.shape) * self.obs.pixel_scale / 2
+            r = max(self.im.data.shape) * self.obs.pixel_scale * 2 / 3
             c = self.geom.radec_predict
+            columns = ','.join([
+                'objname',
+                'objid',
+                'ramean',
+                'decmean',
+                'rameanerr',
+                'decmeanerr',
+                'ndetections',
+                'randomid',
+                'projectionid',
+                'skycellid',
+                'objinfoflag',
+                'qualityflag',
+                'rastack',
+                'decstack',
+                'rastackerr',
+                'decstackerr',
+                'epochmean',
+                'nstackdetections',
+                'ng',
+                'nr',
+                'gqfperfect',
+                'gmeanpsfmag',
+                'gmeanpsfmagerr',
+                'gmeankronmag',
+                'gmeankronmagerr',
+                'gmeanapmag',
+                'gmeanapmagerr',
+                'gflags',
+                'rqfperfect',
+                'rmeanpsfmag',
+                'rmeanpsfmagerr',
+                'rmeankronmag',
+                'rmeankronmagerr',
+                'rmeanapmag',
+                'rmeanapmagerr',
+                'rflags',
+            ])
+
             params = dict(RA=c.ra.deg, DEC=c.dec.deg, SR=r.to(u.deg).value,
-                          max_records=1000)
+                          max_records=3000, ordercolumn1='ndetections',
+                          descending1='on', selectedColumnsCsv=columns)
+            
             q = requests.get('https://archive.stsci.edu/panstarrs/search.php',
                              params=params)
 
             with open(fn, 'w') as outf:
                 outf.write(q.text)
 
-        self.logger.debug('    Reading PS1 catalog from {} .'.format(fn))
-        cat = votable.parse(fn)
-
+        self.logger.info('    Reading PS1 catalog from {} .'.format(fn))
+        cat = votable.parse_single_table(fn)
